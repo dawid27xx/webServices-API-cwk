01b89fbc3f88a6ee352acbf5d9b321286c0893e0
dawid27xx <dawidstepien27@gmail.com>
2025-02-18 16:50:40 +0000

    Initial Setup

---
+++ WScwk/myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+# model for professor
+
+# model for module
+
+# model for module instance
+
+# model for rating
+
+# then migrate the database, and register models in admin panel
---
+++ WScwk/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# implement the api views here:
+
+# register 
+
+# log in 
+
+# logout
+
+# list modules
+
+# view ratings
+
+# view average ratings
+
+# rate professor
+
+# then define the urls in the urls.py file
+
+
