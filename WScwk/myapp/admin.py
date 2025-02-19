d2db4740a9e185d856707ecce7a8ca90371a30f4
dawid27xx <dawidstepien27@gmail.com>
2025-02-19 10:57:32 +0000

    Added DB models

--- WScwk/myapp/admin.py
+++ WScwk/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Professor, Module, ModuleInstance, Rating
 
-# Register your models here.
+admin.site.register(Professor)
+admin.site.register(Module)
+admin.site.register(ModuleInstance)
+admin.site.register(Rating)
--- WScwk/myapp/models.py
+++ WScwk/myapp/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Professor(models.Model):
+    professor_code = models.CharField(max_length=5)
+    full_name = models.CharField(max_length=100)
 
+class Module(models.Model):
+    module_code = models.CharField(max_length=5)
+    module_name = models.CharField(max_length=100)
 
-# model for professor
+class ModuleInstance(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
 
-# model for module
-
-# model for module instance
-
-# model for rating
-
-# then migrate the database, and register models in admin panel
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    module_instance = models.ForeignKey(ModuleInstance, on_delete=models.CASCADE)
+    score = models.IntegerField()
