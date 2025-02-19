497b2a54fd034bc12f8f186e1fe0f8bd03ac30ce
dawid27xx <dawidstepien27@gmail.com>
2025-02-19 11:18:32 +0000

    Finalised the database models

--- WScwk/myapp/models.py
+++ WScwk/myapp/models.py
@@ -5,17 +5,31 @@ class Professor(models.Model):
     professor_code = models.CharField(max_length=5)
     full_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.full_name  
+
 class Module(models.Model):
     module_code = models.CharField(max_length=5)
     module_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.module_code} - {self.module_name}" 
 
 class ModuleInstance(models.Model):
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
     year = models.IntegerField()
     semester = models.IntegerField()
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professors = models.ManyToManyField(Professor)  
+
+    def __str__(self):
+        professor_names = ", ".join([prof.full_name for prof in self.professors.all()])
+        return f"{self.module.module_code} ({self.year}, Semester {self.semester}) - {professor_names}"
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     module_instance = models.ForeignKey(ModuleInstance, on_delete=models.CASCADE)
     score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.module_instance} ({self.score})"
+
